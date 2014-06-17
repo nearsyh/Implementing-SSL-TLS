0e472aed62c6146188757b3753db192336630459
nearsyh <nearsy.h@gmail.com>
2014-06-17 15:52:56 -0400

    add test for base64

--- ch01/test.cpp
+++ ch01/test.cpp
@@ -1,11 +1,10 @@
 #include "base64.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 int main() {
-  string input;
-  cin >> input;
-  cout << base64_encode(input) << endl;
-  cout << base64_decode(base64_encode(input)) << endl;
+  string input = "abcdABCD0123,./+";
+  assert(base64_decode(base64_encode(input)) == input);
   return 0;
 }
